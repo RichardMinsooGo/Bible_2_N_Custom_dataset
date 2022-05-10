f7d9f3942894373b71ec7dcbfde900d27f0d85e5
Richard Minsoo Go <kauntach@naver.com>
2022-05-10 10:32:06 +0900

    Add files via upload

---
+++ 16_TF2_00_baseline_GetBatch_graph_CNN_cifar10.py
@@ -0,0 +1,194 @@
+!pip install --upgrade --no-cache-dir gdown
+
+from IPython.display import clear_output 
+clear_output()
+
+# Step 1 : Git clone Feature map
+
+'''
+# Clone from Github Repository
+! git init .
+! git remote add origin https://github.com/RichardMinsooGo/5_TF2_UCF101_video_classification.git
+! git pull origin master
+# ! git pull origin main
+'''
+
+# Mini-COCO dataset download from Auther's Github repository
+import gdown
+google_path = 'https://drive.google.com/uc?id='
+file_id = '18I06ymkUqKwEon4Dsb8GqkJORwPZZxLd'
+output_name = 'Cifar_10.zip'
+gdown.download(google_path+file_id,output_name,quiet=False)
+# https://drive.google.com/file/d/18I06ymkUqKwEon4Dsb8GqkJORwPZZxLd/view?usp=sharing
+
+% rm -rf sample_data
+!unzip /content/Cifar_10.zip -d /content/data
+clear_output()
+! rm /content/Cifar_10.zip
+
+from glob import glob
+import random
+import os
+import tensorflow as tf
+import numpy as np
+
+def parse_image(filename):
+    image = tf.io.read_file(filename)
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, [IMG_SIZE, IMG_SIZE])
+    return image
+
+IMG_SIZE = 128
+
+X_train, Y_train = [],[]
+path = "/content/data/train"
+classes = os.listdir(path)
+filenames = glob(path + '/*/*')
+random.shuffle(filenames)
+X_train = [parse_image(name) for name in filenames]
+Y_train = [classes.index(name.split('/')[-2]) for name in filenames]
+
+X_train = np.array(X_train) 
+Y_train = np.array(Y_train) 
+
+X_test, Y_test = [],[]
+path = "/content/data/test"
+classes = os.listdir(path)
+filenames = glob(path + '/*/*')
+random.shuffle(filenames)
+X_test = [parse_image(name) for name in filenames]
+Y_test = [classes.index(name.split('/')[-2]) for name in filenames]
+
+X_test = np.array(X_test) 
+Y_test = np.array(Y_test) 
+
+X_train, X_test = X_train / 255.0, X_test / 255.0
+
+train_size = 50
+test_size  = 100
+STEPS = int(len(X_train)/train_size)
+VAL_STEPS = int(len(X_test)/test_size)
+
+# returns batch_size random samples from either training set or validation set
+# resizes each image to (224, 244, 3), the native input size for VGG19
+#Define network
+IMG_SIZE = 224                      # VGG19
+IMG_SHAPE = (IMG_SIZE, IMG_SIZE, 3)
+num_classes = 10                    # cifar10
+
+# 3-Layers Convolution neural network with one hidden layer
+class CNN_Model(Model):
+    def __init__(self):
+        super(CNN_Model, self).__init__()
+        
+        # Convolution 1
+        self.conv1 = Conv2D(64, 3, activation='relu', padding='SAME')
+        
+        # Max pool 1
+        self.maxpool2d1 = MaxPool2D(padding='SAME')
+     
+        # Convolution 2
+        self.conv2 = Conv2D(filters=128, kernel_size=3, activation=tf.nn.relu, padding='SAME')
+        
+        # Max pool 2
+        self.maxpool2d2 = MaxPool2D(padding='SAME')
+        
+        # Convolution 3
+        self.conv3 = Conv2D(filters=256, kernel_size=3, activation=tf.nn.relu, padding='SAME')
+        
+        # Max pool 3
+        self.maxpool2d3 = MaxPool2D(padding='SAME')
+        self.flatten = Flatten()
+        self.d1 = Dense(256, activation='relu')
+        self.d2 = Dropout(0.2)
+        self.d3 = Dense(10, activation='softmax')
+
+    def call(self, x):
+        # Convolution 1
+        x = self.conv1(x)
+        
+        # Max pool 1
+        x = self.maxpool2d1(x)
+        
+        # Convolution 2 
+        x = self.conv2(x)
+        
+        # Max pool 2 
+        x = self.maxpool2d2(x)
+        
+        # Convolution 3
+        x = self.conv3(x)
+        # Max pool 3
+        x = self.maxpool2d3(x)
+        x = self.flatten(x)
+        x = self.d1(x)
+        x = self.d2(x)
+        out = self.d3(x)
+        return out
+
+model = CNN_Model()
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model_name = 'cifar10_CNN_Model'
+
+import cv2
+def getBatch(batch_size, train_or_val='train'):
+    x_batch = []
+    y_batch = []
+    if train_or_val == 'train':
+        idx = np.random.randint(0, len(X_train), (batch_size))
+
+        for i in idx:
+            img = cv2.resize(X_train[i], (IMG_SIZE, IMG_SIZE), interpolation=cv2.INTER_CUBIC)
+            x_batch.append(img)
+            # x_batch.append(X_train[i])
+            y_batch.append(Y_train[i])
+    elif train_or_val == 'val':
+        idx = np.random.randint(0, len(X_test), (batch_size))
+
+        for i in idx:
+            img = cv2.resize(X_test[i], (IMG_SIZE, IMG_SIZE), interpolation=cv2.INTER_CUBIC)
+            x_batch.append(img)
+            # x_batch.append(X_test[i])
+            y_batch.append(Y_test[i]) 
+    else:
+        print("error, please specify train or val")
+
+    x_batch = np.array(x_batch)
+    y_batch = np.array(y_batch)
+    return x_batch, y_batch
+
+from tqdm import tqdm, tqdm_notebook, trange
+EPOCHS = 3
+for epoch in range(EPOCHS):
+
+    with tqdm_notebook(total=STEPS, desc=f"Train Epoch {epoch+1}") as pbar:    
+        train_losses = []
+        train_accuracies = []
+        for s in range(STEPS):
+            x_batch, y_batch = getBatch(train_size, "train")
+            out= model.train_on_batch(x_batch, y_batch)
+            loss_val = out[0]*100
+            acc      = out[1]*100
+
+            train_losses.append(loss_val)
+            train_accuracies.append(acc)
+            
+            pbar.update(1)
+            pbar.set_postfix_str(f"Loss: {loss_val:.4f} ({np.mean(train_losses):.4f}) Acc: {acc:.3f} ({np.mean(train_accuracies):.3f})")
+            
+    with tqdm_notebook(total=VAL_STEPS, desc=f"Test_ Epoch {epoch+1}") as pbar:    
+        test_losses = []
+        test_accuracies = []
+        for s in range(VAL_STEPS):
+            x_batch_val, y_batch_val = getBatch(test_size, "val")
+            evaluation = model.evaluate(x_batch_val, y_batch_val)
+            
+            loss_val= evaluation[0]
+            acc     = evaluation[1]*100
+            
+            test_losses.append(loss_val)
+            test_accuracies.append(acc)
+            pbar.update(1)
+            pbar.set_postfix_str(f"Loss: {loss_val:.4f} ({np.mean(test_losses):.4f}) Acc: {acc:.3f} ({np.mean(test_accuracies):.3f})")
+
+ 
---
+++ 16_TF2_05_transfer_GetBatch_graph_Resnet50_cifar10.py
@@ -0,0 +1,169 @@
+!pip install --upgrade --no-cache-dir gdown
+
+from IPython.display import clear_output 
+clear_output()
+
+# Step 1 : Git clone Feature map
+
+'''
+# Clone from Github Repository
+! git init .
+! git remote add origin https://github.com/RichardMinsooGo/5_TF2_UCF101_video_classification.git
+! git pull origin master
+# ! git pull origin main
+'''
+
+# Mini-COCO dataset download from Auther's Github repository
+import gdown
+google_path = 'https://drive.google.com/uc?id='
+file_id = '18I06ymkUqKwEon4Dsb8GqkJORwPZZxLd'
+output_name = 'Cifar_10.zip'
+gdown.download(google_path+file_id,output_name,quiet=False)
+# https://drive.google.com/file/d/18I06ymkUqKwEon4Dsb8GqkJORwPZZxLd/view?usp=sharing
+
+% rm -rf sample_data
+!unzip /content/Cifar_10.zip -d /content/data
+clear_output()
+! rm /content/Cifar_10.zip
+
+from glob import glob
+import random
+import os
+import tensorflow as tf
+
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Dropout
+from tensorflow.keras import Model, Sequential
+import numpy as np
+
+def parse_image(filename):
+    image = tf.io.read_file(filename)
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, [IMG_SIZE, IMG_SIZE])
+    return image
+
+IMG_SIZE = 128
+
+X_train, Y_train = [],[]
+path = "/content/data/train"
+classes = os.listdir(path)
+filenames = glob(path + '/*/*')
+random.shuffle(filenames)
+X_train = [parse_image(name) for name in filenames]
+Y_train = [classes.index(name.split('/')[-2]) for name in filenames]
+
+X_train = np.array(X_train) 
+Y_train = np.array(Y_train) 
+
+X_test, Y_test = [],[]
+path = "/content/data/test"
+classes = os.listdir(path)
+filenames = glob(path + '/*/*')
+random.shuffle(filenames)
+X_test = [parse_image(name) for name in filenames]
+Y_test = [classes.index(name.split('/')[-2]) for name in filenames]
+
+X_test = np.array(X_test) 
+Y_test = np.array(Y_test) 
+
+X_train, X_test = X_train / 255.0, X_test / 255.0
+
+train_size = 100
+test_size  = 200
+STEPS = int(len(X_train)/train_size)
+VAL_STEPS = int(len(X_test)/test_size)
+
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Dropout, GlobalAveragePooling2D, BatchNormalization
+
+# returns batch_size random samples from either training set or validation set
+# resizes each image to (224, 244, 3), the native input size for VGG19
+#Define network
+IMG_SIZE = 224                      # VGG19
+IMG_SHAPE = (IMG_SIZE, IMG_SIZE, 3)
+num_classes = 10                    # cifar10
+
+def net(num_classes):
+    base_model = tf.keras.applications.ResNet50(input_shape=IMG_SHAPE,
+                                                  include_top=False,
+                                                  weights='imagenet')
+    
+    base_model.trainable = True
+
+    # The last 15 layers fine tune
+    model = Sequential()
+    model.add(base_model)
+    model.add(GlobalAveragePooling2D())
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(.3))
+    model.add(BatchNormalization())
+    model.add(Dense(num_classes, activation='softmax'))
+
+    model.summary()    
+    return model
+
+model = net(num_classes)
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model_name = 'cifar10_ResNet50'
+
+import cv2
+def getBatch(batch_size, train_or_val='train'):
+    x_batch = []
+    y_batch = []
+    if train_or_val == 'train':
+        idx = np.random.randint(0, len(X_train), (batch_size))
+
+        for i in idx:
+            img = cv2.resize(X_train[i], (IMG_SIZE, IMG_SIZE), interpolation=cv2.INTER_CUBIC)
+            x_batch.append(img)
+            # x_batch.append(X_train[i])
+            y_batch.append(Y_train[i])
+    elif train_or_val == 'val':
+        idx = np.random.randint(0, len(X_test), (batch_size))
+
+        for i in idx:
+            img = cv2.resize(X_test[i], (IMG_SIZE, IMG_SIZE), interpolation=cv2.INTER_CUBIC)
+            x_batch.append(img)
+            # x_batch.append(X_test[i])
+            y_batch.append(Y_test[i]) 
+    else:
+        print("error, please specify train or val")
+
+    x_batch = np.array(x_batch)
+    y_batch = np.array(y_batch)
+    return x_batch, y_batch
+
+from tqdm import tqdm, tqdm_notebook, trange
+EPOCHS = 3
+for epoch in range(EPOCHS):
+
+    with tqdm_notebook(total=STEPS, desc=f"Train Epoch {epoch+1}") as pbar:    
+        train_losses = []
+        train_accuracies = []
+        for s in range(STEPS):
+            x_batch, y_batch = getBatch(train_size, "train")
+            out= model.train_on_batch(x_batch, y_batch)
+            loss_val = out[0]
+            acc      = out[1]*100
+
+            train_losses.append(loss_val)
+            train_accuracies.append(acc)
+            
+            pbar.update(1)
+            pbar.set_postfix_str(f"Loss: {loss_val:.4f} ({np.mean(train_losses):.4f}) Acc: {acc:.3f} ({np.mean(train_accuracies):.3f})")
+            
+    with tqdm_notebook(total=VAL_STEPS, desc=f"Test_ Epoch {epoch+1}") as pbar:    
+        test_losses = []
+        test_accuracies = []
+        for s in range(VAL_STEPS):
+            x_batch_val, y_batch_val = getBatch(test_size, "val")
+            evaluation = model.evaluate(x_batch_val, y_batch_val)
+            
+            loss_val= evaluation[0]
+            acc     = evaluation[1]*100
+            
+            test_losses.append(loss_val)
+            test_accuracies.append(acc)
+            pbar.update(1)
+            pbar.set_postfix_str(f"Loss: {loss_val:.4f} ({np.mean(test_losses):.4f}) Acc: {acc:.3f} ({np.mean(test_accuracies):.3f})")
+
+ 
